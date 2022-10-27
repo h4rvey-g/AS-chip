d3bdbd7999f4d63e7f0b51654011ed2bc9aee823
babaolanqiu <babaolanqiu@gmail.com>
2022-10-27 20:34:04 +0800

    ⚡️ Implementing GNU parallel;📝 Finish QC

--- script/01_QC.sh
+++ script/01_QC.sh
@@ -2,15 +2,31 @@
 eval "$(conda shell.bash hook)"
 conda activate fastp
 # create output directory if not exist
-mkdir -p ../data/02_fastq_afterQC
-for i in ../data/01_fastq/*.fastq.gz; do
-    echo "Processing $i"
-    fastp -i $i \
-        -o ../data/02_fastq_afterQC/$(basename $i .fastq.gz).fastq.gz \
-        -h ../data/02_fastq_afterQC/report/$(basename $i .fastq.gz).html \
-        -j ../data/02_fastq_afterQC/report/$(basename $i .fastq.gz).json \
+mkdir -p ../data/02_fastq_afterQC && mkdir -p ../data/02_fastq_afterQC/report
+function getQC() {
+    echo "Processing $1"
+    fastp -i $1 \
+        -o ../data/02_fastq_afterQC/$(basename $1 .fastq.gz).fastq.gz \
+        -h ../data/02_fastq_afterQC/report/$(basename $1 .fastq.gz).html \
+        -j ../data/02_fastq_afterQC/report/$(basename $1 .fastq.gz).json \
         --phred64 \
-        -w 8
-done
+        -f 6 \
+        -Q
+}
+export -f getQC
+parallel getQC ::: ../data/01_fastq/*.fastq.gz --citation
+# for i in ../data/01_fastq/*.fastq.gz; do
+#     echo "Processing $i"
+#     fastp -i $i \
+#         -o ../data/02_fastq_afterQC/$(basename $i .fastq.gz).fastq.gz \
+#         -h ../data/02_fastq_afterQC/report/$(basename $i .fastq.gz).html \
+#         -j ../data/02_fastq_afterQC/report/$(basename $i .fastq.gz).json \
+#         --phred64 \
+#         -w 8
+# done
 # convert fastp json to MultiQC
-multiqc ../data/02_fastq_afterQC/report/ -o ../data/02_fastq_afterQC/report/
+conda activate gcc
+multiqc ../data/02_fastq_afterQC/report/ \
+    -o ../data/02_fastq_afterQC/report/ \
+    -f \
+    -c ../data/02_fastq_afterQC/report/multiqc_config.yaml
