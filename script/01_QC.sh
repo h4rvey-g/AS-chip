e6c3d78b7395907275e70e9eec6237bca4ab0bf2
babaolanqiu <babaolanqiu@gmail.com>
2022-11-03 21:12:03 +0800

    ♻️ Move to trim_galore; deprecate fastp

--- script/01_QC.sh
+++ script/01_QC.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-eval "$(conda shell.bash hook)"
-conda activate fastp
+# eval "$(conda shell.bash hook)"
+# conda activate fastp
 # create output directory if not exist
 mkdir -p ../data/02_fastq_afterQC && mkdir -p ../data/02_fastq_afterQC/report
 function getQC() {
     echo "Processing $1"
-    fastp -i $1 \
-        -o ../data/02_fastq_afterQC/$(basename $1 .fastq.gz).fastq.gz \
-        -h ../data/02_fastq_afterQC/report/$(basename $1 .fastq.gz).html \
-        -j ../data/02_fastq_afterQC/report/$(basename $1 .fastq.gz).json \
+    # use trim_galore to remove adapter, and use trimmomatic to cut first 10bp
+    trim_galore \
         --phred64 \
-        -f 6 \
-        -Q
+        --gzip \
+        --illumina \
+        --clip_R1 10 \
+        --length 20 \
+        --stringency 3 \
+        --fastqc --fastqc_args "-o ../data/02_fastq_afterQC/report" \
+        -o ../data/02_fastq_afterQC $1
 }
 export -f getQC
 parallel getQC ::: ../data/01_fastq/*.fastq.gz
 # convert fastp json to MultiQC
 /data0/apps/anaconda3/bin/multiqc ../data/02_fastq_afterQC/report/ \
     -o ../data/02_fastq_afterQC/report/ \
-    -f \
-    -c ../data/02_fastq_afterQC/report/multiqc_config.yaml
-# use fastqc to check the quality of fastq files, store the results in ../data/02_fastq_afterQC/report_fastqc, and use multiqc to generate a report
-mkdir -p ../data/02_fastq_afterQC/report_fastqc
-fastqc -o ../data/02_fastq_afterQC/report_fastqc ../data/02_fastq_afterQC/*.fastq.gz
-multiqc ../data/02_fastq_afterQC/report_fastqc/ -o ../data/02_fastq_afterQC/report_fastqc/
+    -f
