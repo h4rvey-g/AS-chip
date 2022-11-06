b3edaeceb4bc5157a8087d735343e2a663293edb
babaolanqiu <babaolanqiu@gmail.com>
2022-11-06 16:04:54 +0800

    ♻️ Use fastx_toolkit

--- script/01_QC.sh
+++ script/01_QC.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-# eval "$(conda shell.bash hook)"
-# conda activate fastp
+eval "$(conda shell.bash hook)"
+conda activate fastx
 # create output directory if not exist
 mkdir -p ../data/02_fastq_afterQC && mkdir -p ../data/02_fastq_afterQC/report
+find ../data/02_fastq_afterQC/ -type f -delete
 function getQC() {
     echo "Processing $1"
-    # use trim_galore to remove adapter, and use trimmomatic to cut first 10bp
-    trim_galore \
-        --phred64 \
-        --gzip \
-        --illumina \
-        --clip_R1 10 \
-        --length 20 \
-        --stringency 3 \
-        --fastqc --fastqc_args "-o ../data/02_fastq_afterQC/report" \
-        -o ../data/02_fastq_afterQC $1
+    # first unzip, then use fastx_trimmer to trim the first 10bp
+    gunzip -c $1 | fastx_trimmer -f 10 -z -o ../data/02_fastq_afterQC/$(basename $1)
+    # perform fastqc
+    fastqc ../data/02_fastq_afterQC/$(basename $1) -o ../data/02_fastq_afterQC/report
 }
 export -f getQC
 parallel getQC ::: ../data/01_fastq/*.fastq.gz
