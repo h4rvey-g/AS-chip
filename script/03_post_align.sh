15cdcd7585d492b027e08670eb0ca984cd481136
babaolanqiu <babaolanqiu@gmail.com>
2022-11-09 21:30:21 +0800

    🎨 Improve structure

--- script/03_post_align.sh
+++ script/03_post_align.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # Use samtools to sort and index bam files in 03_align, and store in 04_post_align
 mkdir -p ../data/04_post_align
+mkdir -p ../data/04_post_align/merged
+find ../data/04_post_align/ -type f -delete
+function sort_index {
+    echo "Processing $1"
+    samtools merge -@ 4 -f ../data/04_post_align/$1.bam ../data/03_align/$1*.bam
+}
+export -f sort_index
+cat ../data/sample_list.txt | parallel --jobs 50 sort_index
 function samtools_sort_index() {
     echo "Processing $1"
-    samtools sort -@ 2 -o ../data/04_post_align/$(basename $1 .bam)_sorted.bam $1
-    samtools index -@ 2 ../data/04_post_align/$(basename $1 .bam)_sorted.bam
+    samtools sort -@ 2 -o $1 $1
+    samtools index -@ 2 $1
 }
 export -f samtools_sort_index
-parallel --jobs 50 samtools_sort_index ::: ../data/03_align/*.bam
+parallel --jobs 50 samtools_sort_index ::: ../data/04_post_align/*.bam
 # Use fastqc to check the quality of the bam files, store results in 04_post_align/report, and use multiqc to summarize the results
 mkdir -p ../data/04_post_align/report
 fastqc -t 64 -o ../data/04_post_align/report ../data/04_post_align/*.bam
-multiqc ../data/04_post_align/report -o ../data/04_post_align/report
-# Use a for loop to traverse sample names in sample_list.txt, and use samtools to merge bam files for each sample
-mkdir -p ../data/04_post_align/merged
-for sample in $(cat ../data/sample_list.txt); do
-    echo "Processing $sample"
-    samtools merge -@ 50 ../data/04_post_align/merged/${sample}_merged.bam ../data/04_post_align/${sample}*.bam
-done
+multiqc ../data/04_post_align/report -o ../data/04_post_align/report -f
--- script/04_peak_calling.sh
+++ script/04_peak_calling.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # use macs2 to call peaks for bam in ../data/04_post_align/merged, output to ../data/05_peak_calling
 mkdir -p ../data/05_peak_calling
+find ../data/05_peak_calling/ -type f -delete
 function call_peaks {
     macs2 callpeak -t $1 -f BAM -g mm -n $(basename $1 .ucsc_merged.bam) -B --broad --broad-cutoff 0.1 --extsize 200 --outdir ../data/05_peak_calling
 }
 export -f call_peaks
-parallel --jobs 50 call_peaks ::: ../data/04_post_align/merged/*.bam
+parallel --jobs 50 call_peaks ::: ../data/04_post_align/*.bam
