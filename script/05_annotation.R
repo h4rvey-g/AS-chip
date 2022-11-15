1f4a03f5582561a0b8da067e45226adc72a04d69
babaolanqiu <babaolanqiu@gmail.com>
2022-11-15 22:02:12 +0800

    ♻️ Plot in vanilla R, and include in Rmarkdown

--- script/05_annotation.R
+++ script/05_annotation.R
@@ -15,7 +15,7 @@ options(mc.cores = 40L)
 # opt <- parse_args(OptionParser(option_list = option_list))
 # dir_name <- basename(opt$input) %>% str_extract(".*(?=\\.ucsc.*)") %>% paste0("../data/06_annotation/", .)
 # create ../data/06_annotation/ if not exist
-# if (!dir.exists("./data/06_annotation") dir.create("./data/06_annotation")
+if (!dir.exists("../data/06_annotation")) dir.create("../data/06_annotation")
 # To import broadPeak files
 extraCols_broadPeak <- c(
     signalValue = "numeric", pValue = "numeric",
@@ -36,25 +36,37 @@ anno_list <- mclapply(gr_list, annotatePeak,
 myCluster <- makeCluster(50)
 registerDoParallel(myCluster)
 foreach(i = seq(1, length(gr_list), by = 4), .inorder = TRUE, .packages = "ChIPseeker") %dopar% {
+    pdf(paste0("../data/06_annotation/", names(gr_list)[i], "_cov",".pdf"))
     covplot(gr_list[i:(i + 3)], ylab = "Coverage", xlab = "Genomic Position") + facet_grid(chr ~ .id)
+    dev.off()
 }
-foreach(i = seq(1, length(gr_list), by = 4), .inorder = TRUE, .packages = "ChIPseeker") %dopar% {
+for (i in seq(1, length(gr_list), by = 4)) {
+    pdf(paste0("../data/06_annotation/", names(gr_list)[i], "_heatmap", ".pdf"))
     peakHeatmap(gr_list[i:(i + 3)], color = rainbow(n = 4), TxDb = txdb, upstream = 3000, downstream = 3000) +
         facet_grid(chr ~ .id)
+    dev.off()
 }
 foreach(i = seq(1, length(gr_list), by = 4), .inorder = TRUE, .packages = "ChIPseeker") %dopar% {
+    pdf(paste0("../data/06_annotation/", names(gr_list)[i], "_avgprof", ".pdf"))
     plotAvgProf2(gr_list[i:(i + 3)],
         TxDb = txdb, upstream = 3000, downstream = 3000, ylab = "Read count frequency", xlab = "Genomic Region (5'->3')",
         conf = 0.95, resample = 1000
     ) + facet_grid(chr ~ .id)
+    dev.off()
 }
 foreach(i = seq(1, length(anno_list), by = 1), .inorder = TRUE, .packages = "ChIPseeker") %dopar% {
+    pdf(paste0("../data/06_annotation/", names(anno_list)[i], "_annopie", ".pdf"))
     plotAnnoPie(anno_list[[i]])
+    dev.off()
 }
 foreach(i = seq(1, length(anno_list), by = 1), .inorder = TRUE, .packages = "ChIPseeker") %dopar% {
+    pdf(paste0("../data/06_annotation/", names(anno_list)[i], "_annobar", ".pdf"))
     plotAnnoBar(anno_list[i])
+    dev.off()
 }
 foreach(i = seq(1, length(anno_list), by = 1), .inorder = TRUE, .packages = "ChIPseeker") %dopar% {
+    pdf(paste0("../data/06_annotation/", names(anno_list)[i], "upset", ".pdf"))
     upsetplot(anno_list[[i]], vennpie = TRUE)
+    dev.off()
 }
 stopImplicitCluster()
